1d915b55234427695587292ad63df98b8538f0a9
Bruno Rocha <rochacbruno@gmail.com>
2020-03-25 18:15:45 -0300

    Adding tests

--- dispatch.py
+++ dispatch.py
@@ -1,19 +1,26 @@
+"""Enqueue jobs and test it."""
 import time
 
-from rq import Queue
 from redis import Redis
+from rq import Queue
 
 from tasks import count_words_at_url
 
-
 # Tell RQ what Redis connection to use
-redis_conn = Redis()
-q = Queue(connection=redis_conn)  # no args implies the default queue
+q = Queue(connection=Redis())  # no args implies the default queue
 
-# Delay execution of count_words_at_url('http://nvie.com')
-job = q.enqueue(count_words_at_url, 'https://example.com')
-print(job.result)   # => None
 
-# Now, wait a while, until the worker is finished
-time.sleep(2)
-print(job.result)   # => 889
+def produce_task(url):
+    """Enqueue a task."""
+    # Delay execution of count_words_at_url('http://nvie.com')
+    return q.enqueue(count_words_at_url, url)
+
+
+def test_produce_task():
+    """Ensure task is enqued and resolved, also produces coverage."""
+    # Enqueue a job
+    job = produce_task("http://www.example.com")
+    assert job.result is None
+    # Now, wait a while, until the worker is finished
+    time.sleep(2)
+    assert job.result == 120
--- tasks.py
+++ tasks.py
@@ -1,8 +1,14 @@
+"""Jobs"""
+
 import requests
 
+
 def count_words_at_url(url):
-    import pydevd_pycharm
-    pydevd_pycharm.settrace('localhost', port=29437, stdoutToServer=True, stderrToServer=True)
+    """Count words in a page"""
+    # import pydevd_pycharm
+
+    # pydevd_pycharm.settrace(
+    #   "localhost", port=29437, stdoutToServer=True, stderrToServer=True
+    # )
     resp = requests.get(url)
     return len(resp.text.split())
-
